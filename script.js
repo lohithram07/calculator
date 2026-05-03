18b61451520a3eae5f5d852e972c01fe29d0f758
lohithram07 <lohithramkattunga@gmail.com>
2026-05-03 14:09:59 +0530

    Fully updated calculator with UI

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ function clearDisplay() {
   display.value = "";
 }
 
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
 function calculate() {
-  display.value = eval(display.value);
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
 }
